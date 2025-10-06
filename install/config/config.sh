c97754c16a53e5fc146b55b4fa0999b6294ef8fc
Coby Powers <mail@cobypowers.com>
2025-10-05 20:51:28 -0500

    feat: update user shell to zsh on install

--- install/config/config.sh
+++ install/config/config.sh
@@ -7,3 +7,6 @@ cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
 
 # Use default zshrc from Omarchy
 cp ~/.local/share/omarchy/default/zshrc ~/.zshrc
+
+# Change default user shell
+sudo usermod -s /usr/bin/zsh $OMARCHY_USER_NAME
