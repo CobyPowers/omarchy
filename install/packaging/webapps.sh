7653f8f0d6b511d4cf2b930853f1dcc30b2b626d
Coby Powers <mail@cobypowers.com>
2025-10-19 16:48:57 -0500

    feat: remove all web apps

--- install/packaging/webapps.sh
+++
@@ -1,3 +0,0 @@
-omarchy-webapp-install "Figma" https://figma.com/ Figma.png
-omarchy-webapp-install "Calmyleon" https://calmyleon.com/ myNoise.png
-omarchy-webapp-install "myNoise" https://myNoise.net/ myNoise.png
