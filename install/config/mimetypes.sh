6eb2ddedb7d39c7cbb13531ed29c9b7cb223404a
Coby Powers <mail@cobypowers.com>
2025-10-22 13:59:04 -0500

    feat: officially set firefox as default

--- install/config/mimetypes.sh
+++ install/config/mimetypes.sh
@@ -12,7 +12,7 @@ xdg-mime default imv.desktop image/tiff
 # Open PDFs with the Document Viewer
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
-# Use Chromium as the default browser
+# Use Firefox Developer Edition as the default browser
 xdg-settings set default-web-browser firefox-developer-edition.desktop
 xdg-mime default firefox-developer-edition.desktop x-scheme-handler/http
 xdg-mime default firefox-developer-edition.desktop x-scheme-handler/https
